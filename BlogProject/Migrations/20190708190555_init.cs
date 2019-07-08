7f99c7fe17e6b1ff8f91721b2d37c9995b2ecb29
Pabblo <pawel.mat17@op.pl>
2019-07-08 21:11:42 +0200

    Add project files.

---
+++ BlogProject/Migrations/20190708190555_init.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogProject.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
---
+++ BlogProject/Models/Post.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogProject.Models
+{
+
+    public class BaseContext: DbContext
+    {
+        public BaseContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+    }
+
+
+
+    public class Post
+    {
+        [Key]
+        public int PostId {get;set;}
+        [MaxLength(100)]
+        public string Title { get; set; }
+    }
+}
